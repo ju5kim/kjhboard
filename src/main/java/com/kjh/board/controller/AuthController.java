ba0a6c87ff3871b939f46d0ecb98a40df578be5a
rlawngh8875 <rlawngh8875@gmail.com>
2020-11-09 20:37:19 +0900

    security-context설정으로 로그인 접근하는 예제

--- src/main/java/com/kjh/board/controller/AuthController.java
+++ src/main/java/com/kjh/board/controller/AuthController.java
@@ -1,8 +1,24 @@
 package com.kjh.board.controller;
 
+import org.apache.commons.logging.impl.Log4JLogger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j2;
 
 @Controller
+@RequestMapping("/sample/*")
 public class AuthController {
+	@GetMapping("/all")
+	public void doAll() {
+	}
+	@GetMapping("/member")
+	public void doMember() {
+	}
 	
+	@GetMapping("/admin")
+	public void doAdmin() {
+	}
 }
